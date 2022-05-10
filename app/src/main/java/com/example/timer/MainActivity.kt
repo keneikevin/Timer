9e7a3cb5584eb9c9f308642dfdd0bbfc5b47e015
keneikevin <kkevinkenei@gmail.com>
2022-05-10 11:19:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.timer
+
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.timer.ui.theme.TimerTheme
+import com.example.timer.view.CountDownView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimerTheme {
+                // A surface container using the 'background' color from the theme
+                TransparentStatusBar(windows = window)
+                MyApp()
+            }
+        }
+    }
+}
+
+// Start building your app here!
+@Composable
+fun MyApp() {
+
+    Column(modifier = Modifier
+        .padding(0.dp)
+        .background(Color(0xFF651FFF))
+        .fillMaxWidth()
+    ) {
+
+        CountDownView()
+
+    }
+
+}
+
+@Composable
+fun TransparentStatusBar(windows: Window) =
+    MaterialTheme {
+
+        windows.statusBarColor = MaterialTheme.colors.surface.toArgb()
+        windows.navigationBarColor = MaterialTheme.colors.surface.toArgb()
+
+        @Suppress("DEPRECATION")
+        if (MaterialTheme.colors.surface.luminance() > 0.5f) {
+            windows.decorView.systemUiVisibility = windows.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        @Suppress("DEPRECATION")
+        if (MaterialTheme.colors.surface.luminance() > 0.5f) {
+            windows.decorView.systemUiVisibility = windows.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+    }
+
+@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun LightPreview() {
+    TimerTheme() {
+        MyApp()
+    }
+}
---
+++ app/src/main/java/com/example/timer/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.timer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+val purple200 = Color(0xF14668)
+val purple500 = Color(0xd83f5d)
+val purple700 = Color(0xc03853)
+val teal200 = Color(0xF14668)
